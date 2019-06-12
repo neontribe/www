2ef7a89d4b7940fcd0b0fb7d5c0941c70773f04a
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-12 16:29:19 +0100

    Setup initial layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import 'normalize.css'
 
 import Header from './Header'
+
+const Footer = () => <div style={{ backgroundColor: 'red', height: 150 }} />
+
+const Page = ({ title, children }) => (
+  <>
+    <div className="page-layout">
+      <Header siteTitle={title} />
+      <main role="main">{children}</main>
+      <Footer />
+
+      <style jsx>{`
+        .page-layout {
+          min-height: 100%;
+          display: flex;
+          flex-direction: column;
+        }
+
+        main {
+          flex: 1 0 auto;
+        }
+      `}</style>
+    </div>
+  </>
+)
+
+Page.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+}
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,33 +48,30 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
+        <Helmet title={data.site.siteMetadata.title}>
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <style jsx global>{`
+          html,
+          body,
+          body > div,
+          body > div > div {
+            height: 100%;
+          }
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+        `}</style>
+        <Page title={data.site.siteMetadata.title}>{children}</Page>
       </>
     )}
   />
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
 }
 
 export default Layout
