1b974294f01511b78bfd544a924829d811b64acc
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-14 11:58:37 +0100

    Add hexagon component

--- src/pages/symbols.js
+++ src/pages/symbols.js
@@ -2,14 +2,48 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import ConstrainedWidth from '../components/Layout/ConstrainedWidth'
+import Hexagon from '../components/Hexagon'
+import Text from '../components/Text'
+import logo from '../components/Logo/logo.svg'
+import { COLOUR_NEONS } from '../theme'
 
 export default () => (
   <Layout>
-    <ConstrainedWidth></ConstrainedWidth>
+    <ConstrainedWidth>
+      <h1>
+        <Text>Symbols</Text>
+      </h1>
+      <div className="centered">
+        <div className="hex-container">
+          <h2>
+            <Text>Hexagons</Text>
+          </h2>
+          <div className="hexagon">
+            <Hexagon />
+          </div>
+          <div className="hexagon">
+            <Hexagon src={logo} size={55} />
+          </div>
+          {COLOUR_NEONS.map(c => (
+            <div className="hexagon">
+              <Hexagon size={55} bgColour={c} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </ConstrainedWidth>
     <style jsx>{`
-      .centered {
+      .hex-container {
         display: flex;
         align-items: center;
+      }
+      .hexagon {
+        height: 100px;
+      }
+      .centered {
+        flex-direction: column;
+        display: flex;
+        justify-content: center;
       }
     `}</style>
   </Layout>
