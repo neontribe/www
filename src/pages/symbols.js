dc2e112019294286cf0b47b1d6899c9e5d5cbb0c
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-18 09:49:08 +0100

    Added text examples to symbols page

--- src/pages/symbols.js
+++ src/pages/symbols.js
@@ -6,8 +6,28 @@ import Hexagon from '../components/Hexagon'
 import Text from '../components/Text'
 import logo from '../components/Logo/logo.svg'
 import Heading from '../components/Heading'
-import { COLOUR_NEONS } from '../theme'
+import {
+  COLOUR_NEONS,
+  COLOUR_PRIMARY_BACKGROUND,
+  COLOUR_PRIMARY_BACKGROUND_ALTERNATIVE,
+  COLOUR_SECONDARY_BACKGROUND,
+} from '../theme'
 import Link, { ExternalLink } from '../components/Link'
+
+const TextVariants = () => (
+  <>
+    <Text>Primary Text</Text>
+    <Text alternate>Primary alternate Text</Text>
+    <Text type="secondary">Secondary Text</Text>
+    <Text heavy>Heavy Primary Text</Text>
+    <Text type="primary" transparent>
+      Transparent Primary Text
+    </Text>
+    <Text type="secondary" transparent>
+      Transparent Secondary Text
+    </Text>
+  </>
+)
 
 export default () => (
   <Layout>
@@ -48,6 +68,31 @@ export default () => (
               </ExternalLink>
             </div>
           </div>
+          <div>
+            <Heading level={2} size="medium">
+              <Text>Text</Text>
+            </Heading>
+            <div className="text-wrapper">
+              <p className="primary">
+                <Heading level={3} size="small">
+                  <Text>Background primary</Text>
+                </Heading>
+                <TextVariants />
+              </p>
+              <p className="primary alternate">
+                <Heading level={3} size="small">
+                  <Text alternate>Background primary alternate</Text>
+                </Heading>
+                <TextVariants />
+              </p>
+              <p className="secondary">
+                <Heading level={3} size="small">
+                  <Text type="secondary">Background secondary</Text>
+                </Heading>
+                <TextVariants />
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </ConstrainedWidth>
@@ -63,6 +108,20 @@ export default () => (
         flex-direction: column;
         display: flex;
         justify-content: center;
+      }
+
+      .text-wrapper > p {
+        padding: 1rem;
+      }
+
+      .text-wrapper .primary {
+        background-color: ${COLOUR_PRIMARY_BACKGROUND};
+      }
+      .text-wrapper .primary.alternate {
+        background-color: ${COLOUR_PRIMARY_BACKGROUND_ALTERNATIVE};
+      }
+      .text-wrapper .secondary {
+        background-color: ${COLOUR_SECONDARY_BACKGROUND};
       }
     `}</style>
   </Layout>
