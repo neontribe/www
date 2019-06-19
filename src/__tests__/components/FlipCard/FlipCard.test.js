5f0f23db5645e2cd446d4f584a21790fc6b3625e
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-19 16:20:16 +0100

    Fix tests

---
+++ src/__tests__/components/FlipCard/FlipCard.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import FlipCard, { CardFront, CardBack } from '../../../components/FlipCard'
+
+const TestFlipCard = () => (
+  <FlipCard>
+    <CardFront>front 1</CardFront>
+    <CardBack>back 2</CardBack>
+    Anything else
+  </FlipCard>
+)
+
+test('displays the front and back', () => {
+  const { getByText, queryByText } = render(<TestFlipCard />)
+
+  expect(getByText('front 1')).toBeInTheDocument()
+  expect(getByText('back 2')).toBeInTheDocument()
+  expect(queryByText('Anything else')).toBeNull()
+})
+
+test('default render matches snapshot', () => {
+  const { container } = render(<TestFlipCard />)
+
+  expect(container).toMatchSnapshot()
+})
+
+test('isFlipped render matches snapshot', () => {
+  const { container } = render(<TestFlipCard isFlipped />)
+
+  expect(container).toMatchSnapshot()
+})
