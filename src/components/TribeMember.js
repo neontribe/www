2061a02951a3c67e637c7e97146d8626ba937fe2
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-19 13:36:20 +0100

    Add card flipping component and implemented in tribe member

--- src/components/TribeMember.js
+++ src/components/TribeMember.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
+import css from 'styled-jsx/css'
 
 import {
   COLOUR_SECONDARY_BACKGROUND,
@@ -8,23 +9,33 @@ import {
   CUT_CORNER_PX,
   COLOUR_NEONS,
 } from '../theme'
+import FlipCard, { CardFront, CardBack } from './FlipCard'
 import Heading from './Heading'
 import Text from './Text'
 
 let cardN = 0
-
 const nextNeon = () => COLOUR_NEONS[cardN++ % COLOUR_NEONS.length]
 
-const TribeMember = ({
+const imageStyles = css.resolve`
+  .gatsby-image-wrapper {
+    flex-grow: 1;
+  }
+`
+
+const TribeMemberFront = ({
   fluid,
   name,
   headingLevel,
   skills,
-  bio,
-  neonSeparatorColour = nextNeon(),
+  onClick,
+  sepColour,
 }) => (
-  <figure className="person-card">
-    <Img fluid={fluid} alt={`Picture of ${name}`} />
+  <figure className="tribe-figure" onClick={onClick}>
+    <Img
+      className={imageStyles.className}
+      fluid={fluid}
+      alt={`Picture of ${name}`}
+    />
     <hr className="separator" />
     <figcaption className="name-skills">
       <div className="content">
@@ -36,16 +47,20 @@ const TribeMember = ({
         <Text type="secondary">{skills.join(' | ')}</Text>
       </div>
     </figcaption>
+    {imageStyles.styles}
     <style jsx>{`
       .separator {
-        background-color: ${neonSeparatorColour};
+        background-color: ${sepColour};
         margin: 0;
         height: ${GUTTER_PX}px;
         border: none;
       }
 
-      .person-card {
+      .tribe-figure {
+        height: 100%;
         margin: 0;
+        display: flex;
+        flex-direction: column;
       }
 
       .name-skills {
@@ -66,6 +81,94 @@ const TribeMember = ({
     `}</style>
   </figure>
 )
+
+const TribeMemberBack = ({ onClick, headingLevel, name, skills, bio }) => (
+  <div className="bio" onClick={onClick}>
+    <div className="bio-content">
+      <div role="presentation">
+        <Heading level={headingLevel} size="small">
+          <Text type="secondary" heavy>
+            {name}
+          </Text>
+        </Heading>
+        <Text type="secondary">{skills.join(' | ')}</Text>
+      </div>
+      <p className="back-content">
+        <Text type="secondary">{bio}</Text>
+      </p>
+    </div>
+    <style jsx>{`
+      .bio {
+        height: 100%;
+        padding: ${GUTTER_PX}px;
+        /* Cut the corner off by CUT_CORNER_PX and later on we max-width the content */
+        background: linear-gradient(
+          45deg,
+          transparent ${CUT_CORNER_PX}px,
+          ${COLOUR_SECONDARY_BACKGROUND} ${CUT_CORNER_PX}px
+        );
+      }
+
+      .bio-content {
+        height: 100%;
+        height: calc(100% - ${CUT_CORNER_PX}px);
+        display: flex;
+        flex-direction: column;
+      }
+      .back-content {
+        margin: 0;
+        margin-top: ${2 * GUTTER_PX}px;
+        flex-grow: 1;
+        overflow: auto;
+      }
+    `}</style>
+  </div>
+)
+
+const TribeMember = ({
+  fluid,
+  name,
+  headingLevel,
+  skills,
+  bio,
+  neonSeparatorColour,
+}) => {
+  const [isFlipped, setFlipped] = useState(false)
+  // So this component will remember its colour between re-renders.
+  const [initialNeonColour] = useState(neonSeparatorColour || nextNeon())
+
+  return (
+    <section className="tribe-member">
+      <FlipCard isFlipped={isFlipped}>
+        <CardFront>
+          <TribeMemberFront
+            fluid={fluid}
+            name={name}
+            headingLevel={headingLevel}
+            skills={skills}
+            onClick={() => setFlipped(true)}
+            sepColour={neonSeparatorColour || initialNeonColour}
+          />
+        </CardFront>
+        <CardBack>
+          <TribeMemberBack
+            onClick={() => setFlipped(false)}
+            headingLevel={headingLevel}
+            name={name}
+            skills={skills}
+            bio={bio}
+          />
+        </CardBack>
+      </FlipCard>
+      <style jsx>{`
+        .tribe-member {
+          height: 100%;
+          cursor: pointer;
+        }
+      `}</style>
+    </section>
+  )
+}
 
 TribeMember.propTypes = {
   name: PropTypes.string.isRequired,
--- src/examples/TribeMemberExample.js
+++ src/examples/TribeMemberExample.js
@@ -2,8 +2,16 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Heading from '../components/Heading'
+import Text from '../components/Text'
 import TribeMember from '../components/TribeMember'
 import { GUTTER_PX } from '../theme'
+
+const loremIpsum = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+`
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -30,31 +38,38 @@ export default () => (
       }
     `}
     render={data => (
-      <div className="person-example">
-        <Heading level={2} size="medium" />
-        <div className="person-wrapper">
-          <TribeMember
-            fluid={data.placeholderImage.childImageSharp.fluid}
-            name="Alex Moore"
-            headingLevel={3}
-            skills={['UX', 'HTML/CSS', 'JavaScript']}
-          />
-        </div>
-        <div className="person-wrapper">
-          <TribeMember
-            fluid={data.placeholderImage.childImageSharp.fluid}
-            name="Alex Moore"
-            headingLevel={3}
-            skills={['UX', 'HTML/CSS', 'JavaScript']}
-          />
-        </div>
-        <div className="person-wrapper">
-          <TribeMember
-            fluid={data.placeholderImage.childImageSharp.fluid}
-            name="Alex Moore"
-            headingLevel={3}
-            skills={['UX', 'HTML/CSS', 'JavaScript']}
-          />
+      <div>
+        <Heading level={2} size="medium">
+          <Text>TribeMember</Text>
+        </Heading>
+        <div className="person-example">
+          <div className="person-wrapper">
+            <TribeMember
+              fluid={data.placeholderImage.childImageSharp.fluid}
+              name="Alex Moore"
+              headingLevel={3}
+              skills={['UX', 'HTML/CSS', 'JavaScript']}
+              bio={loremIpsum}
+            />
+          </div>
+          <div className="person-wrapper">
+            <TribeMember
+              fluid={data.placeholderImage.childImageSharp.fluid}
+              name="Alex Moore"
+              headingLevel={3}
+              skills={['UX', 'HTML/CSS', 'JavaScript']}
+              bio={loremIpsum}
+            />
+          </div>
+          <div className="person-wrapper">
+            <TribeMember
+              fluid={data.placeholderImage.childImageSharp.fluid}
+              name="Alex Moore"
+              headingLevel={3}
+              skills={['UX', 'HTML/CSS', 'JavaScript']}
+              bio={loremIpsum}
+            />
+          </div>
         </div>
 
         <style jsx>{`
@@ -64,6 +79,13 @@ export default () => (
           }
           .person-wrapper {
             width: 300px;
+            height: 300px;
+          }
+          .person-wrapper:nth-child(2) {
+            height: 400px;
+          }
+          .person-wrapper:nth-child(3) {
+            height: 500px;
           }
           .person-wrapper + .person-wrapper {
             margin-left: ${3 * GUTTER_PX}px;
