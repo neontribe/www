52696f8069a980d50f1884d55d68db246f48f90c
HedgehogHolly <holly@neontribe.co.uk>
2019-06-25 17:26:09 +0100

    update squiggle seperate

--- src/components/SquiggleSeparator.js
+++ src/components/SquiggleSeparator.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Squiggle from './Squiggle'
 import VerticalSpacing from './VerticalSpacing'
 
-export default ({ color, repeats }) => (
+import { c_NEONS } from '../theme'
+
+export default ({ color = c_NEONS[0], repeats = 5 }) => (
   <div>
     <VerticalSpacing size={10} />
     <Squiggle color={color} repeats={repeats} />
