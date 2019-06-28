304a8ec437132d3754860074d85daaad85dd6c05
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-28 13:55:29 +0100

    Added kara and use flex to lay out grid

--- src/components/TriberGrid.js
+++ src/components/TriberGrid.js
@@ -3,33 +3,53 @@ import { StaticQuery, graphql } from 'gatsby'
 import get from 'lodash.get'
 import TribeMember from './TribeMember'
 import RenderContent from './RenderContent'
-import { GUTTER_PX } from '../theme'
+import { GUTTER_PX, breakpoint } from '../theme'
+
+const makeArrayOfLength = len => Array.apply(null, Array(len))
 
 const TriberGridComponent = ({ tribers }) => (
   <div className="triber-grid">
     {tribers.nodes.map(triber => (
       <div className="triber-container" key={triber.frontmatter.name}>
-        <TribeMember
-          fluid={get(triber, 'frontmatter.image.childImageSharp.fluid')}
-          name={triber.frontmatter.name}
-          headingLevel={2}
-          skills={triber.frontmatter.words}
-          bio={<RenderContent type="secondary" htmlAst={triber.htmlAst} />}
-        />
+        <div className="triber-sizer">
+          <TribeMember
+            fluid={get(triber, 'frontmatter.image.childImageSharp.fluid')}
+            name={triber.frontmatter.name}
+            headingLevel={2}
+            skills={triber.frontmatter.words}
+            bio={<RenderContent type="secondary" htmlAst={triber.htmlAst} />}
+          />
+        </div>
+      </div>
+    ))}
+    {/* Make an odd number appear */}
+    {makeArrayOfLength(3 - (tribers.nodes.length % 3)).map((undef, i) => (
+      <div className="triber-container" key={i}>
+        <div className="triber-sizer" />
       </div>
     ))}
     <style jsx>{`
       .triber-grid {
         display: flex;
         flex-wrap: wrap;
-        justify-content: space-between;
+        justify-content: center;
+        padding: 0 ${GUTTER_PX}px;
         margin-bottom: -${GUTTER_PX * 5}px;
       }
 
-      .triber-container {
+      .triber-sizer {
         width: 300px;
         height: 520px;
+      }
+
+      .triber-container {
         margin-bottom: ${GUTTER_PX * 5}px;
+      }
+
+      @media (${breakpoint('md')}) {
+        .triber-grid {
+          justify-content: space-between;
+        }
       }
     `}</style>
   </div>
