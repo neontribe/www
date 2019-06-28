bfe0181a4450d5741d1ee0c995de722c716b00e7
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-06-28 14:10:28 +0100

    Better grid

--- src/components/TriberGrid.js
+++ src/components/TriberGrid.js
@@ -5,8 +5,6 @@ import TribeMember from './TribeMember'
 import RenderContent from './RenderContent'
 import { GUTTER_PX, breakpoint } from '../theme'
 
-const makeArrayOfLength = len => Array.apply(null, Array(len))
-
 const TriberGridComponent = ({ tribers }) => (
   <div className="triber-grid">
     {tribers.nodes.map(triber => (
@@ -22,34 +20,18 @@ const TriberGridComponent = ({ tribers }) => (
         </div>
       </div>
     ))}
-    {/* Make an odd number appear */}
-    {makeArrayOfLength(3 - (tribers.nodes.length % 3)).map((undef, i) => (
-      <div className="triber-container" key={i}>
-        <div className="triber-sizer" />
-      </div>
-    ))}
     <style jsx>{`
       .triber-grid {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
+        display: grid;
         padding: 0 ${GUTTER_PX}px;
-        margin-bottom: -${GUTTER_PX * 5}px;
+        grid-gap: ${GUTTER_PX * 5}px;
+        grid-template-columns: repeat(auto-fill, 300px);
+        justify-content: center;
       }
 
       .triber-sizer {
         width: 300px;
         height: 520px;
-      }
-
-      .triber-container {
-        margin-bottom: ${GUTTER_PX * 5}px;
-      }
-
-      @media (${breakpoint('md')}) {
-        .triber-grid {
-          justify-content: space-between;
-        }
       }
     `}</style>
   </div>
