ce6d6228cf045f6776893a2e5a6a272396e770ab
Robert Preus-MacLaren <rpprroger@gmail.com>
2019-06-28 16:26:53 +0100

    Update src/pages/what-we-are-doing.js

--- src/pages/what-we-are-doing.js
+++ src/pages/what-we-are-doing.js
@@ -14,7 +14,7 @@ import ExternalLink from '../components/Link'
 import castLogo from '../data/funders/cast-logo.png'
 import comicReliefLogo from '../data/funders/comic-relief-logo.png'
 import mhfLogo from '../data/funders/mhf-logo.png'
-import phfLogo from '../data/funders/phf-logo.png'
+import phfLogo from '../data/funders/PHF-logo.png'
 
 const WhatWeAreDoingPage = () => (
   <Layout>
