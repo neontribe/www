23df642dd1fe1126d6ce0e1436357741b1a33026
Robert Preus-MacLaren <rob@neontribe.co.uk>
2019-07-03 12:47:32 +0100

    Fix safari image problems

--- src/components/Hexagon.js
+++ src/components/Hexagon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Location } from '@reach/router'
 
 let id = 0
 const nextId = () => `image-bg-${id++}`
@@ -8,53 +9,73 @@ const Hexagon = ({ src, bgColour, size, title }) => {
   const id = nextId()
 
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      width="100%"
-      height="100%"
-      xlink="http://www.w3.org/1999/xlink"
-      viewBox="0 0 300 300"
-    >
-      {title && <title>{title}</title>}
-      <defs>
-        <pattern
-          id={id}
-          x="0"
-          y="0"
-          height="300"
-          width="300"
-          patternUnits="userSpaceOnUse"
+    <Location>
+      {locationProps => (
+        <svg
+          className="hexagon-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width="100%"
+          height="100%"
+          xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 300 300"
         >
-          <rect
-            className="hex-background"
-            x="0"
-            y="0"
-            height="300"
-            width="300"
-          ></rect>
-          {src && (
-            <image
-              x={`${(100 - size) / 2}%`}
-              y={`${(100 - size) / 2}%`}
-              width={`${size}%`}
-              height={`${size}%`}
-              href={src}
-            />
-          )}
-        </pattern>
-      </defs>
-      <polygon
-        className="hex"
-        points="280,75 150,0 20,75 20,225 150,300 280,225"
-        fill={`url('#${id}')`}
-      ></polygon>
-      <style jsx>{`
-        .hex-background {
-          fill: ${bgColour};
-        }
-      `}</style>
-    </svg>
+          {title && <title>{title}</title>}
+          <defs>
+            <pattern
+              id={id}
+              x="0"
+              y="0"
+              height="300"
+              width="300"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                className="hex-background"
+                x="0"
+                y="0"
+                height="300"
+                width="300"
+              ></rect>
+              {src && (
+                <image
+                  x={`${(100 - size) / 2}%`}
+                  y={`${(100 - size) / 2}%`}
+                  width={`${size}%`}
+                  height={`${size}%`}
+                  href={src}
+                />
+              )}
+            </pattern>
+          </defs>
+          <polygon
+            className="hex"
+            points="280,75 150,0 20,75 20,225 150,300 280,225"
+            fill={`url('${locationProps.location.href}#${id}')`}
+          ></polygon>
+          <style jsx>{`
+            .hexagon-svg {
+              height: 100%;
+              width: 100%;
+              display: block;
+            }
+
+            .hex-background {
+              fill: ${bgColour};
+            }
+          `}</style>
+          <style>{`
+            object {
+              width: 100%;
+              display: block;
+              height: auto;
+              position: relative;
+              padding-top: 100%;
+            }
+          `}</style>
+        </svg>
+      )}
+    </Location>
   )
 }
 
