94eb419e7825bf24e0252c019d70a229038929f4
LearnFast <learnfastlearn@gmail.com>
2019-11-27 13:14:01 +0100

    removed width from the background-image property

--- src/components/Squiggle/index.js
+++ src/components/Squiggle/index.js
@@ -17,7 +17,7 @@ const Squiggle = ({ color, repeats }) => (
         background-position-x: 0;
         background-position-y: center;
         background-repeat: repeat-x;
-        background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='100%25' height='8px' viewBox='0 0 9 6' enable-background='new 0 0 9 6' xml:space='preserve'%3e%3cpolygon stroke='%23fb37f1' points='4.5,4.5 0,0 0,1.208 4.5,5.708 9,1.208 9,0 '/%3e%3c/svg%3e");
+        background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' height='8px' viewBox='0 0 9 6' enable-background='new 0 0 9 6' xml:space='preserve'%3e%3cpolygon stroke='%23fb37f1' points='4.5,4.5 0,0 0,1.208 4.5,5.708 9,1.208 9,0 '/%3e%3c/svg%3e");
         background-size: 12px 12px;
       }
     `}</style>
