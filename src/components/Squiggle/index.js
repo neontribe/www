e76326331b4230e6dfd17a54e6cac0c6faf0b8c3
Nikomus <nick.wade@live.co.uk>
2019-12-10 13:12:32 +0000

    Reinstate use of variables

--- src/components/Squiggle/index.js
+++ src/components/Squiggle/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SQUIGGLE_WIDTH = 18
-
+const SQUIGGLE_HEIGHT = (SQUIGGLE_WIDTH / 3) * 2
 
 const Squiggle = ({ color, repeats }) => (
   <>
@@ -17,8 +17,12 @@ const Squiggle = ({ color, repeats }) => (
         background-position-x: 0;
         background-position-y: center;
         background-repeat: repeat-x;
-        background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' height='8px' viewBox='0 0 9 6' enable-background='new 0 0 9 6' xml:space='preserve'%3e%3cpolygon stroke='%23fb37f1' points='4.5,4.5 0,0 0,1.208 4.5,5.708 9,1.208 9,0 '/%3e%3c/svg%3e");
-        background-size: 12px 12px;
+        background-image: url('data:image/svg+xml;utf-8,<?xml version="1.0" encoding="UTF-8"?>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="${SQUIGGLE_HEIGHT}px" width="${SQUIGGLE_WIDTH}px" viewBox="0 0 9 6" enable-background="new 0 0 9 6" xml:space="preserve">
+          <polygon stroke="${encodeURIComponent(
+            color
+          )}" points="4.5,4.5 0,0 0,1.208 4.5,5.708 9,1.208 9,0 "/>
+        </svg>');
       }
     `}</style>
   </>
