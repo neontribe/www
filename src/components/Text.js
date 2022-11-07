d902fb6cef0c903716e4807d2d981e0b814305e3
hannahouazzane <hannahouazzane@gmail.com>
2022-11-07 23:08:40 +0000

    add: default props to avoid injections

--- src/components/Text.js
+++ src/components/Text.js
@@ -4,7 +4,6 @@ import classNames from 'classnames'
 
 import { fontSizes } from '../theme'
 
-// TODO: Should we have a block level version which uses a 0 margin p?
 const Text = ({
   children,
   weight,
@@ -12,11 +11,6 @@ const Text = ({
   align,
   color,
   hyphenation = false,
-  lineHeight,
-  maxCharacter,
-  paddingRight,
-  paddingLeft,
-  paddingTop,
 }) => (
   <div className={classNames('text', hyphenation && 'hyphenation')}>
     {children}
@@ -25,14 +19,9 @@ const Text = ({
       .text {
         ${weight ? `font-weight: ${weight};` : ''}
         ${fontSizes[size] ? `font-size: ${fontSizes[size]};` : ''}
-        ${lineHeight ? `line-height: ${lineHeight};` : ''}
+
         ${align ? `text-align: ${align};` : ''}
         ${color ? `color: ${color};` : ''}
-        ${paddingLeft ? `padding-left: ${paddingLeft};` : ''}
-        ${paddingTop ? `padding-top: ${paddingTop};` : ''}
-        ${paddingRight ? `padding-right: ${paddingRight};` : ''}
-
-        ${maxCharacter ? `max-width: ${maxCharacter};` : ''}
       }
 
       .hyphenation {
@@ -51,11 +40,14 @@ Text.propTypes = {
   lineHeight: PropTypes.number,
   size: PropTypes.oneOf(Object.keys(fontSizes)),
   color: PropTypes.string,
-  maxCharacter: PropTypes.string,
-  whiteSpace: PropTypes.string,
-  paddingLeft: PropTypes.string,
-  paddingTop: PropTypes.string,
-  paddingRight: PropTypes.string,
+}
+//default values to avoid any injections?
+Text.defaultProps = {
+  weight: '400',
+  align: 'inherit',
+  lineHeight: 'inherit',
+  size: 'inherit',
+  color: 'inheirt',
 }
 
 export default Text
