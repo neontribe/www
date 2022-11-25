6c45068e1fb865403753d237d4af24bf5027acea
hannahouazzane <hannahouazzane@gmail.com>
2022-11-25 13:34:36 +0000

    add: ability for header to change colour onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
-import React from 'react'
+// To do:
+// Make the mobile bar look good
+// Do logic to show one or the other depending on window size
+
+import React, { useState, useEffect } from 'react'
 import css from 'styled-jsx/css'
 
 import { breakpoint, c_NAV_ACTIVE, FONT_SECONDARY } from '../theme'
 import { InternalLink } from './Link'
 import Text from './Text'
 import ConstrainedWidth from './Layout/ConstrainedWidth'
-
+import VerticalSpacing from '../components/VerticalSpacing'
 import logo from './logo.svg'
+import classNames from 'classnames'
 
 const activeLinkStyles = css.resolve`
   a {
     border-bottom: 4px solid ${c_NAV_ACTIVE};
-
     color: #48e9ce;
   }
 `
@@ -29,109 +33,287 @@ const NavLink = ({ children, active, ...props }) => (
   </Text>
 )
 
-const Nav = () => (
-  <nav className="nav">
-    <ul className="list">
-      <li>
-        <NavLink active to="/our-work">
-          Our work
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/the-tribe">The tribe</NavLink>
-      </li>
-      <li>
-        <NavLink to="/contact-us">Contact us</NavLink>
-      </li>
-    </ul>
+const DesktopNav = () => {
+  return (
+    <nav className="nav">
+      <ul className="list">
+        <li>
+          <NavLink active to="/our-work">
+            Our Work
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/the-tribe">The tribe</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact-us">Contact</NavLink>
+        </li>
+      </ul>
 
-    <style jsx>{`
-      .list {
-        margin: 0;
-        padding: 0;
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        flex-direction: row;
-        list-style: none;
-        width: 100%;
-        font-size: 20px;
-      }
-
-      .list > * + * {
-        margin-top: 0;
-        margin-left: 3rem;
-      }
-
-      @media (max-width: 530px) {
+      <style jsx>{`
+        
+        .nav {
+          display:flex;
+        }
         .list {
-          flex-direction: column;
           margin: 0;
+          padding: 0;
+          display: flex;
+          align-items: center;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          flex-direction: row;
+          width: auto;
+     
+
+          list-style: none;
+
+          font-size: 20px;
         }
 
         .list > * + * {
           margin-top: 0;
-          margin-left: 0;
-        }
-      }
-    `}</style>
-  </nav>
-)
-
-const Header = () => (
-  <ConstrainedWidth mobilePaddingBottom="0rem">
-    <header className="header">
-      <div>
-        <InternalLink to="/" title="Link to Neontribe homepage">
-          <img className="logo" src={logo} alt="Neontribe" />
-        </InternalLink>
-      </div>
-
-      <div className="nav-wrapper">
-        <Nav />
-      </div>
-    </header>
-
-    <style jsx>{`
-      .header {
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-        margin: 1rem -1rem 0;
-        font-family: ${FONT_SECONDARY};
-        width: 100%;
-      }
-
-      .header > * {
-        display: flex;
-        flex: 1 0 auto;
-
-        align-items: center;
-        margin: 1rem;
-      }
-
-      .nav-wrapper {
-        display: flex;
-        justify-content: flex-end;
-      }
-
-      .logo {
-        height: 2rem;
-      }
-
-      @media (max-width: 860px) {
-        .header {
-          display: block;
+          margin-left: 3rem;
         }
 
-        .header > * {
-          flex-grow: 0;
+      
+
+
+        @media (min-width:500px) and (max-width:700px) {
+          .list {
+          justify-content:flex-start;
+          
+        }
+      `}</style>
+    </nav>
+  )
+}
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="testing">
+      <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        &#8801;
+      </button>
+      <nav className="mobile-nav">
+        {isOpen && (
+          <ul className="list">
+            <li className="first-item">
+              <NavLink active to="/what-we-are-doing">
+                Our Work
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/the-tribe">The tribe</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact-us">Contact</NavLink>
+            </li>
+          </ul>
+        )}
+      </nav>
+
+      <style jsx>{`
+        .testing {
+          display: flex;
           flex-direction: column;
-          align-items: flex-start;
+          justify-content: flex-start;
+          left: 0.6rem;
+
+          width: 96%;
+          position: absolute;
+          align-items: center;
+          height: 100%;
+          z-index: 1;
         }
-      }
-    `}</style>
-  </ConstrainedWidth>
-)
+
+        li:not(:first-child) {
+          padding-top: 3rem;
+        }
+
+        .list {
+          background-color: purple;
+          padding-right: 2rem;
+
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+          align-items: center;
+          justify-content: flex-start;
+          padding-top: 50%;
+          width: 100%;
+          list-style: none;
+        }
+
+        .mobile-nav {
+          width: 100%;
+          margin-right: 0.2rem;
+          height: 100%;
+          max-height: 100%;
+        }
+
+        .hamburger {
+          background: none;
+          color: white;
+          border: none;
+          font-size: 3rem;
+          margin: 0;
+          padding-right: 1rem;
+          display: flex;
+          align-self: flex-end;
+        }
+      `}</style>
+    </div>
+  )
+}
+const isBrowser = typeof window !== 'undefined'
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  // Hook to detect screen size and returns true if screen is a desktop/tablet size and false if it is a mobile
+  console.log({ isOpen })
+  function checkScreenWidth() {
+    if (isBrowser) {
+      return window.innerWidth
+    }
+  }
+
+  const [windowSize, setWindowSize] = useState(checkScreenWidth())
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(checkScreenWidth())
+    }
+
+    window.addEventListener('resize', handleWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [])
+
+  return (
+    <div className={classNames('introduction', isOpen && 'other')}>
+      <ConstrainedWidth>
+        <header className="header">
+          <div className="logo-container">
+            <InternalLink to="/" title="Link to Neontribe homepage">
+              <img className="logo" src={logo} alt="Neontribe" />
+            </InternalLink>
+          </div>
+          {windowSize > 500 && <DesktopNav />}
+          {windowSize < 500 && (
+            <div className="testing">
+              <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                &#8801;
+              </button>
+              <nav className="mobile-nav">
+                {isOpen && (
+                  <ul className="list">
+                    <li className="first-item">
+                      <NavLink active to="/what-we-are-doing">
+                        Our Work
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/the-tribe">The tribe</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/contact-us">Contact</NavLink>
+                    </li>
+                  </ul>
+                )}
+              </nav>
+            </div>
+          )}
+        </header>
+      </ConstrainedWidth>
+      <VerticalSpacing size={5} />
+      <style jsx>{`
+        .introduction {
+          background-color: black;
+        }
+
+        .other {
+          background-color: purple;
+        }
+
+        .header {
+          display: flex;
+          justify-content: space-between;
+          margin: 1rem -1rem 0;
+          font-family: ${FONT_SECONDARY};
+        }
+
+        @media (max-width: 700px) and (min-width: 500px) {
+          .header {
+            display: block;
+          }
+        }
+
+        .logo-container {
+          height: 4rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+        }
+
+        .logo {
+          height: 2rem;
+        }
+
+        .testing {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          left: 0.6rem;
+
+          width: 96%;
+          position: absolute;
+          align-items: center;
+          height: 100%;
+          z-index: 1;
+        }
+
+        li:not(:first-child) {
+          padding-top: 3rem;
+        }
+
+        .list {
+          background-color: purple;
+          padding-right: 2rem;
+
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+          align-items: center;
+          justify-content: flex-start;
+          padding-top: 50%;
+          width: 100%;
+          list-style: none;
+        }
+
+        .mobile-nav {
+          width: 100%;
+          margin-right: 0.2rem;
+          height: 100%;
+          max-height: 100%;
+        }
+
+        .hamburger {
+          background: none;
+          color: white;
+          border: none;
+          font-size: 3rem;
+          margin: 0;
+          padding-right: 1rem;
+          display: flex;
+          align-self: flex-end;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Header
--- src/components/Layout/Page.js
+++ src/components/Layout/Page.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { c_TEXT_DARK } from '../../theme'
+
 import Header from '../Header'
 import Footer from '../Footer'
 
-import VerticalSpacing from '../VerticalSpacing'
-
 const Page = ({ title, children }) => (
   <div className="page-layout">
-    <div className="introduction">
-      <Header siteTitle={title} />
-
-      <VerticalSpacing size={5} />
-    </div>
+    <Header siteTitle={title} />
 
     <main role="main">{children}</main>
 
@@ -23,10 +17,6 @@ const Page = ({ title, children }) => (
         min-height: 100%;
         display: flex;
         flex-direction: column;
-      }
-
-      .introduction {
-        background-color: ${c_TEXT_DARK};
       }
 
       main {
