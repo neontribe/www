3597b9eb596d97a15c30662f37d16ed6db16c8c7
hannahouazzane <hannahouazzane@gmail.com>
2022-11-25 15:29:11 +0000

    add: privacy page styling

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -5,10 +5,41 @@ import { StaticQuery, graphql } from 'gatsby'
 import ConstrainedWidth from '../components/Layout/ConstrainedWidth'
 import { c_PRIMARY_BACKGROUND } from '../theme'
 import PageMeta from '../components/PageMeta'
+import PageTop from '../components/PageTop'
+import Text from '../components/Text'
+import Container from '../components/Container'
+import VerticalSpacing from '../components/VerticalSpacing'
+import circleArc from '../components/circleArc.svg'
 
-const NotFoundPage = () => (
+const PrivacyPolicy = () => (
   <Layout>
     <PageMeta title="Privacy" />
+
+    <PageTop>
+      <ConstrainedWidth>
+        <Container justifyContent="space-between" mobileFlexDirection="column">
+          <Text size="xlarge" color="#48e9ce">
+            <h1>Neontribe Privacy Policy </h1>
+          </Text>
+          <VerticalSpacing size={3} />
+
+          <div className="heading-arc">
+            <img src={circleArc} height={128} width={277} />{' '}
+          </div>
+        </Container>
+      </ConstrainedWidth>
+      <style jsx>{`
+        @media (max-width: 860px) {
+          .heading-arc {
+            padding-right: 0;
+            padding-top: 2rem;
+            align-self: center;
+          }
+        }
+      `}</style>
+    </PageTop>
+    <VerticalSpacing size={5}></VerticalSpacing>
+
     <StaticQuery
       query={graphql`
         query {
@@ -38,4 +69,4 @@ const NotFoundPage = () => (
   </Layout>
 )
 
-export default NotFoundPage
+export default PrivacyPolicy
