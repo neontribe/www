65262eada6322e65713ab51fd177c36056e075c7
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2024-06-17 10:19:48 +0100

    feat: generate case study pages from markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,61 @@
-const path = require(`path`)
+const path = require('path')
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        limit: 1000
-        filter: { fields: { sourceName: { eq: "blog-posts" } } }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-            fields {
-              sourceName
+
+  // General function to create pages
+  async function createPagesFromSource(sourceName, pathPrefix, templatePath) {
+    const template = path.resolve(templatePath)
+    const result = await graphql(`
+      {
+        allMarkdownRemark(
+          limit: 1000
+          filter: { fields: { sourceName: { eq: "${sourceName}" } } }
+        ) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
+              fields {
+                sourceName
+              }
             }
           }
         }
       }
-    }
-  `)
+    `)
 
-  if (result.errors) {
-    throw result.errors
+    if (result.errors) {
+      throw result.errors
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: `${pathPrefix}/${node.frontmatter.slug}`,
+        component: template,
+        context: {
+          slug: node.frontmatter.slug,
+        },
+      })
+    })
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: `blog/${node.frontmatter.slug}`,
-      component: blogPostTemplate,
-      context: {
-        slug: node.frontmatter.slug,
-      },
-    })
-  })
+  // Create pages for case studies
+  await createPagesFromSource(
+    'case-studies',
+    'case-studies',
+    'src/templates/case-study/index.js'
+  )
+
+  // Create pages for blog posts
+  await createPagesFromSource(
+    'blog-posts',
+    'blog',
+    'src/templates/blog-post.js'
+  )
 }
+
 // Support IE11 when running gatsby develop
 exports.onCreateWebpackConfig = function onCreateWebpackConfig({
   actions,
