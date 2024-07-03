448c60c8a38bd7bf211e869090fe7faf7bfb543d
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2024-07-03 15:16:42 +0100

    feat: improve style on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,30 +1,65 @@
 import React from 'react'
-
 import Layout from '../components/Layout'
 import ConstrainedWidth from '../components/Layout/ConstrainedWidth'
-import Text from '../components/Text'
+import VerticalSpacing from '../components/VerticalSpacing'
 import PageMeta from '../components/PageMeta'
+import Container from '../components/Container'
+import FullScreenHeader from '../components/FullScreenHeader'
+import StyledLink from '../components/Button'
+
+import arcs from '../components/arcs.svg'
+import { fontSizes } from '../theme'
 
 const NotFoundPage = () => (
   <Layout>
-    <PageMeta title="Not Found" />
-    <ConstrainedWidth>
-      <div className="accessibility-centered">
-        <h1>404 Page Not Found</h1>
+    <PageMeta title="Page not found" />
+    <FullScreenHeader>
+      <ConstrainedWidth>
+        <Container justifyContent="space-around" mobileFlexDirection="column">
+          <div className="page-not-found-text">
+            <h1>Sorry this page does not exist.</h1>
 
-        <p>
-          <Text size="large">
-            <i>++?????++ Out of Cheese Error. Redo From Start.</i>
-          </Text>
-        </p>
-      </div>
-    </ConstrainedWidth>
+            <VerticalSpacing size={5} />
+
+            <StyledLink connect="/" background="black" border_color="#48e9ce">
+              Return to home page
+            </StyledLink>
+          </div>
+          <img
+            src={arcs}
+            height={200}
+            width={200}
+            alt="Decorative neon pink & blue arc icon"
+          />
+        </Container>
+
+        <VerticalSpacing size={6} />
+      </ConstrainedWidth>
+    </FullScreenHeader>
 
     <style jsx>{`
-      .accessibility-centered {
-        text-align: center;
-        display: flex;
-        flex-direction: column-reverse;
+      .page-not-found-text {
+        max-width: 700px;
+      }
+
+      h1 {
+        font-size: ${fontSizes['title']};
+      }
+
+      img {
+        align-self: center;
+      }
+
+      @media (max-width: 860px) {
+        h1 {
+          font-size: ${fontSizes['xlarge']};
+        }
+        .page-not-found-text {
+          display: flex;
+          flex-direction: column;
+          text-align: center;
+          align-items: center;
+        }
       }
     `}</style>
   </Layout>
